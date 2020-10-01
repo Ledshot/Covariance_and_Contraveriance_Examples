50d461d3e62927cf0bd5de571306677e3aefde41
ChristopherSanderson <47638403+ledshot@users.noreply.github.com>
2020-10-01 10:05:48 +0100

    C# Program
    C# program

---
+++ Program/model.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryIt
+{
+    public interface IEntity
+    {
+        bool IsValid();
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public class Employee : Person  , IEntity
+    {
+        public int Id { get; set; }
+
+        public bool IsValid()
+        {
+            return true;
+        }
+
+        public virtual void DoWork()
+        {
+            Console.WriteLine("Doing real work");
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public virtual void DoWork()
+        {
+            Console.WriteLine("Creating a meeting");
+        }
+    }
+}
